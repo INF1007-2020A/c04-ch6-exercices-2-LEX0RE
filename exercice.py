c67f75ef0e224a74d4e2cecfbd8ce6f8bbfffeb6
Alexandre Gélinas <gelinas.alexandre.2001@gmail.com>
2020-10-05 18:12:18 -0400

    Update exercice.py
    Reduce of code

--- exercice.py
+++ exercice.py
@@ -6,16 +6,14 @@ from matplotlib.colors import cnames
 
 def list_to_dict(some_list: list) -> dict:
     # TODO: Transformer la liste en dictionnaire, les éléments de la liste deviennent les clés et leur index deviennent les valeurs
-    dict_list = {some_list[key]: key for key in range(len(some_list))}
-    return dict_list
+
+    return {some_list[key]: key for key in range(len(some_list))}
 
 
 def color_name_to_hex(colors: list) -> list:
     # TODO: Trouver la valeur hex de chaque couleur dans la liste et créer une liste de tupple où le premier élément est le nom de la couleur et le deuxième est la valeur hex
-    color_list = []
-    for color in colors:
-        color_list.append((color, cnames[color]))
-    return color_list
+
+    return [(color, cnames[color]) for color in colors]
 
 
 def create_list() -> list:
